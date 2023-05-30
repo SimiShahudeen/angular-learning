5e61aab969287d68da8dd75e0d9eb554519b8b62
simi <alchemy.officialz@gmail.com>
2023-05-30 17:59:01 +0530

    first commit

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { OnSameUrlNavigation } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  data="Your perfect banking partner"
+  constructor(){}
+    ngOnInit(): void{}
+    placeholderData="enter your password"
+
+login(){
+  alert("login clicked")
+}
+unameChange(event:any){
+console.log(event.target.value);
+
+}
+}
