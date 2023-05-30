6c5635677ae413e576c52f5856f5853c1d9d3068
simi <alchemy.officialz@gmail.com>
2023-05-30 20:25:34 +0530

    second commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -7,17 +7,18 @@ import { OnSameUrlNavigation } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  uname: any
+  data = "Your perfect banking partner"
+  constructor() { }
+  ngOnInit(): void { }
+  placeholderData = "enter your password"
 
-  data="Your perfect banking partner"
-  constructor(){}
-    ngOnInit(): void{}
-    placeholderData="enter your password"
+  login() {
+    alert("login clicked")
+  }
+  unameChange(event: any) {
+    // console.log(event.target.value);
+    const uname = event.target.value
 
-login(){
-  alert("login clicked")
-}
-unameChange(event:any){
-console.log(event.target.value);
-
-}
+  }
 }
